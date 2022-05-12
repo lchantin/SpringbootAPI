7bc2e90a277a47b229f6ff085bd46b1a60a3bcff
LauraCh <laura.chantin@gmail.com>
2022-05-12 12:31:09 +0200

    Configurations to start server and embedded bdd H2

--- src/main/java/laura/example/springboot/SpringbootapiApplication.java
+++ src/main/java/laura/example/springboot/SpringbootapiApplication.java
@@ -1,9 +1,13 @@
 package laura.example.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
 public class SpringbootapiApplication {
 
 	public static void main(String[] args) {
